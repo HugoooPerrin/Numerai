8b79d24e8bfef01ecdb5937bb64cf1d23f79a0bf
HugoooPerrin <b00605838@essec.edu>
2018-06-16 12:23:55 +0200

    jordanan:2657 (58), ken: 2322 (66), charles: 2460 (75)

--- main/running.py
+++ main/running.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
 
     names = ['bernie', 'jordan', 'ken', 'elizabeth', 'charles']
 
-    for name in ['bernie']: #names:
+    for name in ['elizabeth']: #names:
 
         print('\n----------------------------  {}  ----------------------------'.format(name.upper()))
 
@@ -40,7 +40,7 @@ if __name__ == '__main__':
 
         stacking = Numerai(week=111, name=name)
 
-        stacking.load_data(stageNumber=1, evaluate=True)
+        stacking.load_data(stageNumber=1, evaluate=False)
 
 
     #=========================================================================================================
@@ -50,9 +50,9 @@ if __name__ == '__main__':
         stacking.kmeansTrick(k=15, stage=[2], interaction=False)
         stacking.PCA(n_components=2, stage=[1], interaction=True)
         # stacking.meanEncoding()
-        stacking.autoEncoder(stage=[1], interaction=False,
-                             layers=[25, 10, 25], dropout=0.5, learningRate=0.00002, batch=64, epoch=4,
-                             cvNumber=3, displayStep=500, useGPU=True, evaluate=False)
+        # stacking.autoEncoder(stage=[1], interaction=False,
+        #                      layers=[25, 10, 25], dropout=0.5, learningRate=0.00002, batch=64, epoch=4,
+        #                      cvNumber=3, displayStep=500, useGPU=True, evaluate=False)
 
 
     #=========================================================================================================
@@ -64,32 +64,32 @@ if __name__ == '__main__':
 
 
     ## DEEP LEARNING
-        stacking.trainingNN(layers=[160,50], dropout=0.6, learningRate=0.00001, batch=64, epoch=5,
-                            cvNumber=3, displayStep=500, useGPU=useGPU, evaluate=False)
+        # stacking.trainingNN(layers=[160,50], dropout=0.6, learningRate=0.00001, batch=64, epoch=5,
+        #                     cvNumber=3, displayStep=500, useGPU=useGPU, evaluate=False)
 
 
-    # ## MACHINE LEARNING
-    #     stacking.training(nCores, models)
+    ## MACHINE LEARNING
+        stacking.training(nCores, models)
 
 
-    # #=========================================================================================================
-    # #================================ 4. COMPILATION
+    #=========================================================================================================
+    #================================ 4. COMPILATION
 
 
-    # ## MACHINE LEARNING
-    #     stacking.compile(nCores, neuralNetworkCompiler=False)
+    ## MACHINE LEARNING
+        stacking.compile(nCores, neuralNetworkCompiler=False)
 
 
-    # ## DEEP LEARNING
-    #     # stacking.compile(nCores, neuralNetworkCompiler=True, 
-    #     #                  hidden=20, dropout=0.5, learningRate=0.000001, batch=32, epoch=5, 
-    #     #                  cvNumber=1, displayStep=2000, useGPU=useGPU)
+    ## DEEP LEARNING
+        # stacking.compile(nCores, neuralNetworkCompiler=True, 
+        #                  hidden=20, dropout=0.5, learningRate=0.000001, batch=32, epoch=5, 
+        #                  cvNumber=1, displayStep=2000, useGPU=useGPU)
 
 
-    # #=========================================================================================================
-    # #================================ 5. PREDICTION
+    #=========================================================================================================
+    #================================ 5. PREDICTION
 
 
-    #     stacking.submit(submissionNumber=1, week=111)
+        stacking.submit(submissionNumber=2, week=111)
 
 
